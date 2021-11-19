11c60639b1b9530c8767cfa7292d76aa48055713
abailey76 <abailey76@duck.com>
2021-11-18 20:25:46 -0600

    first push of ch 2 studio walkthru

---
+++ AreaOfCircleDemo/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaOfCircleDemo
+{
+    public static class Circle
+    {
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI* Math.Pow(radius, 2);
+        }
+
+        public static double CalculateCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double CalculateDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public static double CalculateFuelConsumption(double radius, double milesPerGallon)
+        {
+            return CalculateCircumference(radius) / milesPerGallon;
+        }
+    }
+}
---
+++ AreaOfCircleDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaOfCircleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Please enter a radius: ");
+            input = Console.ReadLine();
+            double eval;
+
+            if (!double.TryParse(input, out eval) || input == "")
+            {
+                Console.WriteLine("I am throwing a tantrum because you did not feed me! >:(");
+                return;
+            }
+            
+            double radius = double.Parse(input);
+            
+            while (radius < 0)
+            {
+                Console.WriteLine("Hey that's not a positive number! Enter a positive radius: ");
+                radius = double.Parse(Console.ReadLine());
+            }
+           
+            Console.WriteLine("Please enter miles per gallon: ");
+            double milesPerGallon = double.Parse(Console.ReadLine());
+            double fuelConsumed = Circle.CalculateFuelConsumption(radius, milesPerGallon);
+
+            Console.WriteLine("It will take " + fuelConsumed + " gallons of gas to go around the circle.");
+
+
+
+        }
+    }
+}
